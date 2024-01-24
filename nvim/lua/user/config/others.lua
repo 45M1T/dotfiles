f04cfe1c3feac16940b9570721a31c4fbfe09a9f
45M1T <shahiasmit@proton.me>
2024-01-24 21:28:00 +0545

    nvim

---
+++ nvim/lua/user/config/others.lua
@@ -0,0 +1,9 @@
+return {
+    'nvim-tree/nvim-web-devicons',
+    {
+        "windwp/nvim-autopairs",
+        config = function()
+            require("nvim-autopairs").setup {}
+        end
+    }
+}
