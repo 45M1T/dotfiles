f4d4447a80de2ed2a237e45a491dc119d08baf49
45M1T <shahiasmit@proton.me>
2024-01-24 20:49:26 +0545

    deleted:    alacritty/alacritty.toml 	deleted:    kitty/kitty.conf 	deleted:    nvim/init.lua 	deleted:    nvim/lazy-lock.json 	deleted:    nvim/lua/user/config/cmp.lua 	deleted:    nvim/lua/user/config/color.lua 	deleted:    nvim/lua/user/config/harpoon.lua 	deleted:    nvim/lua/user/config/indentline.lua 	deleted:    nvim/lua/user/config/lsp.lua 	deleted:    nvim/lua/user/config/lualine.lua 	deleted:    nvim/lua/user/config/others.lua 	deleted:    nvim/lua/user/config/telescope.lua 	deleted:    nvim/lua/user/config/treesitter.lua 	deleted:    nvim/lua/user/keymaps.lua 	deleted:    nvim/lua/user/lazy.lua 	deleted:    nvim/lua/user/options.lua 	deleted:    st 	deleted:    suckless/dwm/LICENSE 	deleted:    suckless/dwm/Makefile 	deleted:    suckless/dwm/README 	deleted:    suckless/dwm/config.def.h 	deleted:    suckless/dwm/config.def.h.orig 	deleted:    suckless/dwm/config.def.h.rej 	deleted:    suckless/dwm/config.h 	deleted:    suckless/dwm/config.mk 	deleted:    suckless/dwm/drw.c 	deleted:    suckless/dwm/drw.h 	deleted:    suckless/dwm/drw.o 	deleted:    suckless/dwm/dwm 	deleted:    suckless/dwm/dwm.1 	deleted:    suckless/dwm/dwm.c 	deleted:    suckless/dwm/dwm.c.orig 	deleted:    suckless/dwm/dwm.c.rej 	deleted:    suckless/dwm/dwm.o 	deleted:    suckless/dwm/dwm.png 	deleted:    suckless/dwm/fibonacci.c 	deleted:    suckless/dwm/patches/dwm-alpha-20230401-348f655.diff 	deleted:    suckless/dwm/patches/dwm-alternativetags-6.3.diff 	deleted:    suckless/dwm/patches/dwm-alttagsdecoration-2020010304-cb3f58a.diff 	deleted:    suckless/dwm/patches/dwm-alwayscenter-20200625-f04cac6.diff 	deleted:    suckless/dwm/patches/dwm-autostart-20161205-bb3bd6f.diff 	deleted:    suckless/dwm/patches/dwm-colorbar-6.2.diff 	deleted:    suckless/dwm/patches/dwm-fibonacci-5.8.2.diff 	deleted:    suckless/dwm/patches/dwm-fullgaps-6.4.diff 	deleted:    suckless/dwm/patches/dwm-notitle-6.2.diff 	deleted:    suckless/dwm/transient.c 	deleted:    suckless/dwm/util.c 	deleted:    suckless/dwm/util.h 	deleted:    suckless/dwm/util.o

--- nvim/lua/user/config/others.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'nvim-tree/nvim-web-devicons',
-    {
-        "windwp/nvim-autopairs",
-        config = function()
-            require("nvim-autopairs").setup {}
-        end
-    }
-}
